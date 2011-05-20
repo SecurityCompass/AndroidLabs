96a196fead426520e7b92c83103a27004e152bbe
Ewan Sinclair <somebloke789@gmail.com>
2011-05-20 16:39:39 -0400

    Added menus to the setup screens

--- src/com/securitycompass/labs/falsesecuremobile/SetLocalPasswordActivity.java
+++ src/com/securitycompass/labs/falsesecuremobile/SetLocalPasswordActivity.java
@@ -18,6 +18,9 @@ import android.content.Intent;
 import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -104,6 +107,54 @@ public class SetLocalPasswordActivity extends Activity {
             i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
             startActivity(i);
         }
+    }
+    
+    /** Called when an item is selected from the options menu */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case R.id.optionsmenu_reset:
+            resetApplication();
+            return true;
+        case R.id.optionsmenu_preferences:
+            launchPreferenceScreen();
+            return true;
+        default:
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+    /** Creates an options menu */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+        return true;
+    }
+
+    /** Resets the application */ 
+    private void resetApplication() {
+        BankingApplication ba = (BankingApplication) getApplication();
+        ba.clearStatements();
+        Editor e = ba.getSharedPrefs().edit();
+        e.clear();
+        e.commit();
+        ba.lockApplication();
+        launchLoginScreen();
+    }
+
+    /** Launches the preferences screen */
+    private void launchPreferenceScreen() {
+        Intent i = new Intent(this, EditPreferencesActivity.class);
+        startActivity(i);
+    }
+    
+    /** Launches the accounts screen, doing any necessary processing first */
+    private void launchLoginScreen() {
+        Intent launchLogin = new Intent(this, LoginActivity.class);
+        launchLogin.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(launchLogin);
     }
     
 }
