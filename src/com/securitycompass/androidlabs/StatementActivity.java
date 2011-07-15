3aeac5bcb165fc1b95212e65184e417ee7d2406a
Maxim Veytsman <maxim@ontoillogical.com>
2011-07-14 20:13:42 -0400

    updated branch to have correct application/package name

--- src/com/securitycompass/androidlabs/StatementActivity.java
+++ src/com/securitycompass/androidlabs/StatementActivity.java
@@ -2,7 +2,7 @@
  * Copyright 2011 Security Compass
  */
 
-package com.securitycompass.labs.falsesecuremobile;
+package com.securitycompass.androidlabs.advancedencryptionsolution;
 
 import java.io.File;
 import java.io.FileInputStream;
